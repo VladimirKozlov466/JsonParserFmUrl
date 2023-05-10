f7b8ff1fcdb1dc26b7be2c37378de08210aa12f6
VladimirKozlov466 <vladimirkozlov466@icloud.com>
2023-05-10 22:11:56 +0500

    6th commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,10 +22,23 @@ public class Main {
 
         Map<String, Float> rateOnlyMap = JsonFmUrl.getSimpleMap(fullMap);
 
+
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите название валюты, чтобы узнать ее курс по отношению RUR" +
+//                "В формате кода валюты (Например QAR) или название (например Китайский юань)");
+//
+//        String currencyName = in.next();
+//
+//        if (rateOnlyMap.containsKey(currencyName)) {
+//            System.out.println("Курс " + currencyName + " = " + rateOnlyMap.get(currencyName));
+//        } else {
+//            System.out.println("Такой валюты нет в списке. Проверьте название");
+//        }
+
         Float parsedFloat = rateOnlyMap.get("USD");
         Float parsedFloat1 = rateOnlyMap.get("доллар сша");
         System.out.println("Курс USD = " + parsedFloat);
-        System.out.println("EUR = " + parsedFloat1);
+        System.out.println("доллар сша = " + parsedFloat1);
 
     }
 }
