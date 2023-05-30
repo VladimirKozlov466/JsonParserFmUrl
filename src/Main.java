c5997303b76a75c78fac5eeed2ac906a5e50bcf4
VladimirKozlov466 <vladimirkozlov466@icloud.com>
2023-05-30 23:21:08 +0500

    14th commit release version with new interface

--- src/Main.java
+++ src/Main.java
@@ -5,33 +5,90 @@ import java.util.*;
 
 
 public class Main {
+
+    public static String timeDate;
     public static void main(String[] args) throws IOException {
 
+        //Парсим js файл с курсами валют Центробанка России по url  в JSON объект
         JSONObject parsedFromUrl = JsonFmUrl.getJsonByUrl();
 
+        //По ключу (атрибуту) "Valute" получаем значение в нашем случае новый JSON объект, содержащий только валюты
         JSONObject jsonCurrenciesOnly = parsedFromUrl.getJSONObject("Valute");
 
-        String timeDate = parsedFromUrl.get("Timestamp").toString();
+        //По ключу (атрибуту) "Timestamp" получаем значение времени, публикации курсов Центробанком
+        timeDate = parsedFromUrl.get("Timestamp").toString();
 
+        /* Парсим в HashMap JSON объект c курсами валют, где ключ это код валюты (например "USD")
+        а значение это объект с атрибутами валюты*/
         Map<String, Object> fullMap = JsonFmUrl.jsonToMap(jsonCurrenciesOnly);
 
+        /*Парсим в новую HashMap только курсы валют, где ключ типа String, содержащий код валюты (например "USD")
+        или наименование валюты на русском языке типа "Туркменский манат"
+         */
         Map<String, Float> rateOnlyMap = JsonFmUrl.getSimpleMap(fullMap);
 
+        mainMenu(rateOnlyMap);
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите название валюты, чтобы узнать ее курс по отношению RUR " +
-                "в формате кода валюты (Например QAR) или название (например Китайский юань или доллар или Рупий)");
-
-        String currencyName = in.nextLine();
-
-        Map<String, Float> matchedCurrencies = JsonFmUrl.matchedCurrenciesMap(rateOnlyMap, currencyName);
-
-
-        JsonFmUrl.printFoundCurrencies(matchedCurrencies);
-
-        System.out.println("Данные Центробанка на " + timeDate);
-
-        JsonFmUrl.writeFoundMatchesToExcel(matchedCurrencies);
+//        Scanner in = new Scanner(System.in);
+//
+//
+//        System.out.println("Введите название валюты, чтобы узнать ее курс по отношению RUR " +
+//                "в формате кода валюты (Например QAR) или название (например Китайский юань или доллар или Рупий)");
+//
+//        String currencyName = in.nextLine();
+//
+//        Map<String, Float> matchedCurrencies = JsonFmUrl.matchedCurrenciesMap(rateOnlyMap, currencyName);
+//
+//
+//        JsonFmUrl.printFoundCurrencies(matchedCurrencies);
+//
+//        System.out.println("Данные Центробанка на " + timeDate);
+//
+//        JsonFmUrl.writeFoundMatchesToExcel(matchedCurrencies);
 
     }
+    public static void mainMenu(Map<String, Float> rateOnlyMap) {
+        Scanner in = new Scanner(System.in);
+        int selectOption;
+        System.out.println("""
+                Выберете пункт меню
+                1 - Найти и показать текущий курс валюты
+                2 - Найти курс валюты, покзать и сохранить в Excel файл
+                3 - Выход из программы""");
+
+        selectOption = in.nextInt();
+
+        switch (selectOption) {
+            case 1:
+                System.out.println("Введите название валюты, чтобы узнать ее курс по отношению RUB " +
+                        "в формате кода валюты (Например QAR) или название (например Китайский юань" +
+                        " или доллар или Рупий и тп)");
+                Scanner newIn = new Scanner(System.in);
+                String currencyName = newIn.nextLine();
+                Map<String, Float> matchedCurrencies = JsonFmUrl.matchedCurrenciesMap(rateOnlyMap, currencyName);
+                JsonFmUrl.printFoundCurrencies(matchedCurrencies);
+                System.out.println("Данные Центробанка на " + timeDate);
+                mainMenu(rateOnlyMap);
+            case 2:
+                System.out.println("Введите название валюты, чтобы узнать ее курс по отношению RUB " +
+                        "в формате кода валюты (Например QAR) или название (например Китайский юань" +
+                        " или доллар или Рупий и тп)");
+                Scanner newNewIn = new Scanner(System.in);
+                String currencyName1 = newNewIn.nextLine();
+                Map<String, Float> matchedCurrencies1 = JsonFmUrl.matchedCurrenciesMap(rateOnlyMap, currencyName1);
+                JsonFmUrl.printFoundCurrencies(matchedCurrencies1);
+                System.out.println("Данные Центробанка на " + timeDate);
+                try {
+                    JsonFmUrl.writeFoundMatchesToExcel(matchedCurrencies1);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                mainMenu(rateOnlyMap);
+            case 3:
+                System.exit(0);
+            default:
+                System.out.println("Введено не корректное значение");
+        }
+    }
+
 }
